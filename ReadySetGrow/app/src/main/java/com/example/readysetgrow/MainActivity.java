7fff47e9128b87438bcc54c20f8dbff59a311a53
Jeannie Q <jeanniequach01@gmail.com>
2020-11-21 17:39:18 -0500

    Account creation
    Created a 'create an account w/ app' page; connected front page w/ 'create an account' page

--- ReadySetGrow/app/src/main/java/com/example/readysetgrow/MainActivity.java
+++ ReadySetGrow/app/src/main/java/com/example/readysetgrow/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.readysetgrow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SignUp = (Button) findViewById(R.id.SignUp);
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignUpPage();
+            }
+        });
+    }
+
+    public void openSignUpPage(){
+        Intent intent = new Intent(this, SignUpPage.class);
+        startActivity(intent);
     }
 }
