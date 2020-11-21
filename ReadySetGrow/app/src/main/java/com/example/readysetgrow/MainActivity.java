cbf997ddab7a8fd38ad4ab82791e44d6e4211ca2
RX <R.@Richards-MacBook-Pro.local>
2020-11-20 23:55:04 -0500

    added extra page

--- ReadySetGrow/app/src/main/java/com/example/readysetgrow/MainActivity.java
+++ ReadySetGrow/app/src/main/java/com/example/readysetgrow/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.readysetgrow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.SignUp);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
     }
+
+    public void openActivity2() {
+        Intent intent;
+        intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+
 }
