23de5ef661b96c07d6998de49f042cec67a9822b
Jeannie Q <jeanniequach01@gmail.com>
2020-11-30 16:13:42 -0500

    Making it work (connecting pages together)

--- ReadySetGrow/app/src/main/java/com/example/readysetgrow/LogIn.java
+++ ReadySetGrow/app/src/main/java/com/example/readysetgrow/LogIn.java
@@ -1,12 +1,12 @@
 package com.example.readysetgrow;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LogIn extends AppCompatActivity {
     private Button Backspace;
--- ReadySetGrow/app/src/main/java/com/example/readysetgrow/SignUpPage.java
+++ ReadySetGrow/app/src/main/java/com/example/readysetgrow/SignUpPage.java
@@ -1,11 +1,11 @@
 package com.example.readysetgrow;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpPage extends AppCompatActivity {
     private Button Login2;
