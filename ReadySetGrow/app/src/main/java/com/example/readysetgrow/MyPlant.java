0520314ffce20c197451ee97b9a01d209ae50be3
RX <R.@Richards-MacBook-Pro.local>
2020-11-30 19:59:33 -0500

    finished my pages

--- ReadySetGrow/app/src/main/java/com/example/readysetgrow/MyPlant.java
+++ ReadySetGrow/app/src/main/java/com/example/readysetgrow/MyPlant.java
@@ -2,13 +2,30 @@ package com.example.readysetgrow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MyPlant extends AppCompatActivity {
+    private ImageButton back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_plant);
+
+        back_button = (ImageButton) findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBack();
+            }
+        });
+    }
+
+    public void openBack(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
