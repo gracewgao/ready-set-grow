8c6ef9c261d27091b6835b9f01313fe111916ead
RX <R.@Richards-MacBook-Pro.local>
2020-12-02 15:19:29 -0500

    i need some editing magic

--- ReadySetGrow/app/src/main/java/com/example/readysetgrow/MyPlant2.java
+++ ReadySetGrow/app/src/main/java/com/example/readysetgrow/MyPlant2.java
@@ -6,24 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Button;
 
-public class MyPlant extends AppCompatActivity {
+public class MyPlant2 extends AppCompatActivity {
     private ImageButton back_button;
-    private Button water;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_plant);
-
-        water = (Button) findViewById(R.id.water);
-        water.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNewPlant();
-            }
-        });
+        setContentView(R.layout.activity_my_plant2);
 
         back_button = (ImageButton) findViewById(R.id.back_button);
         back_button.setOnClickListener(new View.OnClickListener() {
@@ -32,10 +22,6 @@ public class MyPlant extends AppCompatActivity {
                 openBack();
             }
         });
-    }
-    public void openNewPlant(){
-        Intent intent = new Intent(this, MyPlant2.class);
-        startActivity(intent);
     }
 
     public void openBack(){
